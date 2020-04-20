9a54e8d1d78c8cf89bf1703fb4a196e23c8aa803
malmons <malmons@mail.ru>
2020-04-20 20:48:36 +0300

    Add module css

--- my-app/src/Components/Header.js
+++ my-app/src/Components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
         <div className={h.appheader}>
             <header>
                 {/*<img src="https://pugai.ru/wp-content/uploads/2019/05/popugaj-korella-drozhit.jpg"/>*/}
-                <img src="https://images.vexels.com/media/users/3/138590/isolated/preview/b202a8891eebbca3554606e9888b60c1-tropischer-vogel-silhouette-by-vexels.png" alt={}/>
+                <img src="https://images.vexels.com/media/users/3/138590/isolated/preview/b202a8891eebbca3554606e9888b60c1-tropischer-vogel-silhouette-by-vexels.png" alt=""/>
             </header>
         </div>
     );
--- my-app/src/Components/MainContent.js
+++ my-app/src/Components/MainContent.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className={c.appcontent}>
             <header>
-                <img src="https://avatars.mds.yandex.net/get-pdb/1245924/3f504d50-049b-40ca-9bf7-9b9d1c3b2346/s1200" alt{}/>
+                <img src="https://avatars.mds.yandex.net/get-pdb/1245924/3f504d50-049b-40ca-9bf7-9b9d1c3b2346/s1200" alt=""/>
             </header>
             <div className={c.avatar}>
                 <img src="https://cs8.pikabu.ru/post_img/2017/08/01/7/1501586649182953144.jpg" alt=""/>
